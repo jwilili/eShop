8fd337d0235b703b133ff2f96985f2b54797a4f5
abdeljalil.OUZZINE <34714128+jwilili@users.noreply.github.com>
2019-05-06 10:53:41 +0000

    update seetings

--- shopback/shop/shop/settings.py
+++ shopback/shop/shop/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hyo89g=q2-=95n$631k#ikx^h&o1&#^=nuoz^0tl@tf^%@$lp6'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
